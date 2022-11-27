733afa879c5b102062ae2883b21e9a6696e2b2bb
Krixlion <konstanty.cyran.k@gmail.com>
2022-11-27 14:02:13 +0100

    Extract env loading to seperate package

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"regexp"
+	"os/signal"
+	"syscall"
 
-	"github.com/joho/godotenv"
 	"github.com/krixlion/dev-forum_article/cmd/service"
+	"github.com/krixlion/dev-forum_article/pkg/env"
 )
 
 // Hardcoded root dir name.
 const projectDir = "app"
 
-// LoadEnv assumes the root directory name is "app" and
-// the .env file is located in the root directory.
-// It panics if it cannot find the file named ".env" in the root dir.
-func loadEnv() {
-	re := regexp.MustCompile(`^(.*` + projectDir + `)`)
-	cwd, _ := os.Getwd()
-	rootPath := re.Find([]byte(cwd))
-
-	err := godotenv.Load(string(rootPath) + `/.env`)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to load .env, err: %s", err))
-	}
-}
-
 func main() {
-	loadEnv()
+	env.Load(projectDir)
 	service.Run()
+
+	signalChannel := make(chan os.Signal, 2)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+
+	sig := <-signalChannel
+	switch sig {
+	case os.Interrupt:
+		//handle SIGINT
+	case syscall.SIGTERM:
+		//handle SIGTERM
+	case syscall.SIGKILL:
+		service.Close()
+		os.Exit(0)
+	}
 }
