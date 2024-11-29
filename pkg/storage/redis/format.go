508148fcd7c9119c711ed7f498f9860fd4a853cd
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2024-11-29 16:15:31 +0100

    docs(redis): update func descriptions

--- pkg/storage/redis/format.go
+++ pkg/storage/redis/format.go
@@ -15,8 +15,11 @@ type scanCmder interface {
 }
 
 // scan enhances the Scan method of a scanCmder with these features:
-//   - it returns the error redis.Nil when the key does not exist. See https://github.com/go-redis/redis/issues/1668
-//   - it supports embedded struct better. See https://github.com/go-redis/redis/issues/2005#issuecomment-1019667052
+//   - Returns redis.Nil when the key does not exist ([#1668]).
+//   - Provides support for embedded structs ([#2005]).
+//
+// [#1668]: https://github.com/go-redis/redis/issues/1668
+// [#2005]: https://github.com/go-redis/redis/issues/2005#issuecomment-1019667052
 func scan(s scanCmder, dest ...interface{}) error {
 	switch cmd := s.(type) {
 	case *redis.MapStringStringCmd:
@@ -46,6 +49,7 @@ func addPrefix(prefix, key string) string {
 }
 
 // mapArticle converts an article to a map. Formats time.Time fields to RFC3339.
+// Supports only time.Time and string fields. Fields of other types are omitted.
 func mapArticle(article entity.Article) map[string]string {
 	v := reflect.ValueOf(article)
 	values := make(map[string]string)
@@ -56,7 +60,7 @@ func mapArticle(article entity.Article) map[string]string {
 		fieldName := str.ToLowerSnakeCase(v.Type().Field(i).Name)
 
 		// Format time.Time fields to RFC3339.
-		// Redis-go doesn't support time.Time fields scanning.
+		// Redis-go doesn't support time.Time scanning.
 		if fType == reflect.TypeOf(time.Time{}) {
 			values[fieldName] = field.Interface().(time.Time).Format(time.RFC3339)
 			continue
